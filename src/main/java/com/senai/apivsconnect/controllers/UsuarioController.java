f6dbf461dd528c24459fa711cf27c57fca3fca86
Thiago-Nascimento <thiago.r.nascimento@hotmail.com>
2023-10-26 08:28:39 -0300

    Aula 25/10

---
+++ src/main/java/com/senai/apivsconnect/controllers/UsuarioController.java
@@ -0,0 +1,40 @@
+package com.senai.apivsconnect.controllers;
+
+import com.senai.apivsconnect.models.UsuarioModel;
+import com.senai.apivsconnect.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(value = "/usuarios", produces = {"application/json"})
+public class UsuarioController {
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioModel>> listarUsuarios() {
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.findAll());
+    }
+
+    @GetMapping("/{idUsuario}")
+    public ResponseEntity<Object> exibirUsuario(@PathVariable(value = "idUsuario") UUID id) {
+        Optional<UsuarioModel> usuarioBuscado = usuarioRepository.findById(id);
+
+        if (usuarioBuscado.isEmpty()) {
+            // Retornar usuario não encontrado
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioBuscado.get());
+    }
+
+}
